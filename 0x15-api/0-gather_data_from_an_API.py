eb66e9115377ec6d8c3cfcd9c77c058453473612
zyadhany <zeyad.hany2003@gmail.com>
2024-04-30 01:07:21 +0300

    solved task

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" get api to do """
+import sys
+import requests
+
+
+def main():
+    """ main function """
+
+    id = sys.argv[1]
+    name = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(id)).json()['name']
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos/?userId={}"
+                        .format(id)).json()
+
+    done = []
+
+    for task in todo:
+        if task['completed'] is True:
+            done.append(task['title'])
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, len(done), len(todo)))
+
+    for task in done:
+        print("\t", task, sep="")
+
+
+if __name__ == "__main__":
+    main()
