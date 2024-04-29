7c94e4b28da85dc04ed643c6980f3736ea49a020
zyadhany <zeyad.hany2003@gmail.com>
2024-04-30 01:12:08 +0300

    solved task

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" get api to do """
+import csv
+import requests
+import sys
+
+
+def main():
+    """ main function """
+
+    id = sys.argv[1]
+    name = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(id)).json()['username']
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos/?userId={}"
+                        .format(id)).json()
+
+    with open('{}.csv'.format(id), 'w+') as file:
+        for todo in todos:
+            info = '"{}","{}","{}","{}"\n'.format(
+                id, name, todo.get('completed'), todo.get('title'))
+            file.write(info)
+
+
+if __name__ == "__main__":
+    main()
