2e034e357ad7f005c6be27f1ee1fa432de2310ec
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-06-04 19:26:35 +0000

    #0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,14 @@ import json
 import requests
 import sys
 
-
 def number_of_subscribers(subreddit):
     """reddit API, return number subscribers """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    client = requests.session()
-    r = client.get(url, allow_redirects=False)
-    if r.status_code == 200:
-        return (r.json()["data"]["subscribers"])
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    client = requests.get(url, headers=headers)
+    
+    print(client)
+    if client.status_code == 200:
+        return client.json()["data"]["subscribers"]
     else:
-        return(0)
+        return 0
